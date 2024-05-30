6713c138f4c98f4085bc564aab767e44671e4735
unknown <nbertschmann@gmail.com>
2024-05-29 21:36:46 -0400

    Added Insufficient Funds test and Negative Transfer Amount Test

--- src/TransferService.cpp
+++ src/TransferService.cpp
@@ -1,16 +1,22 @@
 #include "TransferService.h"
 #include <iostream>
+#include <stdexcept>
 
 TransferService::TransferService(BankAccount& sender, BankAccount& recipient) : sender_(sender), recipient_(recipient) {}
 
 void TransferService::transfer(double amount) {
     
-    if(sender_.getBalance()-amount >= 0){
-        sender_.withdraw(amount);
-        recipient_.deposit(amount);
+    
+    if(amount < 0){
+        throw std::runtime_error("Transfer amount is below 0");
+    }
+    
+    else if(sender_.getBalance() - amount < 0){
+        throw std::runtime_error("Insufficient balance in sender account");
     }
 
     else{
-        std::cout << "Insufficient balance in sender account";
+        sender_.withdraw(amount);
+        recipient_.deposit(amount);
     }
 }
--- tests/TransferServiceTest.cpp
+++ tests/TransferServiceTest.cpp
@@ -7,32 +7,84 @@ using namespace ::testing;
 
 class MockBankAccount : public BankAccount {
     public:
+        
         MOCK_METHOD(double, getBalance, (), (const, override));
         MOCK_METHOD(void, deposit, (double), (override));
         MOCK_METHOD(void, withdraw, (double), (override));
 };
 
-TEST(TransferServiceTest, MoneyTransferTest)
-{
+TEST(TransferServiceTest, MoneyTransferTest) {
+    // Create mock bank accounts
     MockBankAccount sender;
     MockBankAccount recipient;
-   
-    double recipientInitialBalance = 1000;
-    double senderInitialBalance = 1650;
-    double transferAmount = 450;
+
+    // Set expectations on mock objects
+    double senderInitialBalance = 1000.0;
+    double recipientInitialBalance = 500.0;
+    double transferAmount = 100.0;
 
     EXPECT_CALL(sender, getBalance()).WillOnce(Return(senderInitialBalance));
 
-    EXPECT_CALL(sender, withdraw(transferAmount));
-    EXPECT_CALL(recipient, deposit(transferAmount));
+    EXPECT_CALL(sender, withdraw(transferAmount)).Times(1);
+    EXPECT_CALL(recipient, deposit(transferAmount)).Times(1);
 
+    // Create TransferService instance with mock accounts
     TransferService transferService(sender, recipient);
+
+    // Perform money transfer
     transferService.transfer(transferAmount);
 
-    double senderFinalBalance = senderInitialBalance - transferAmount;
-    double recipientFinalBalance = recipientFinalBalance + transferAmount;
+    // Verify balances after transfer
+    double expectedSenderBalance = senderInitialBalance - transferAmount;
+    double expectedRecipientBalance = recipientInitialBalance + transferAmount;
 
-    EXPECT_EQ(senderFinalBalance, sender.getBalance());
-    EXPECT_EQ(recipientFinalBalance, recipient.getBalance());
+    EXPECT_CALL(sender, getBalance()).WillOnce(Return(expectedSenderBalance));
+    EXPECT_CALL(recipient, getBalance()).WillOnce(Return(expectedRecipientBalance));
+
+    EXPECT_EQ(expectedSenderBalance, sender.getBalance());
+    EXPECT_EQ(expectedRecipientBalance, recipient.getBalance());
+}
+
+TEST(TransferServiceTest, InsufficientFundsTest){
+    
+    MockBankAccount sender;
+    MockBankAccount recipient;
+
+    // Set expectations on mock objects
+    double senderInitialBalance = 50.0;
+    double recipientInitialBalance = 500.0;
+    double transferAmount = 100.0;
+
+    EXPECT_CALL(sender, getBalance()).WillOnce(Return(senderInitialBalance));
+    
+    // Do not expect withraw or deposit to be called
+    EXPECT_CALL(sender, withdraw(_)).Times(0);
+    EXPECT_CALL(recipient, deposit(_)).Times(0);
+
+    TransferService transferService(sender, recipient);
+    
+    // Expect std::runtime error to be thrown due to insufficient funds
+    EXPECT_THROW(transferService.transfer(transferAmount), std::runtime_error);
+
+}
+
+TEST(TransferServiceTest, NegativeTransferAmountTest)
+{
+    double senderInitialBalance = 1000;
+    double recipientInitialBalance = 1000;
+    double negativeTransferAmount = -200.5;
+    
+    MockBankAccount sender;
+    MockBankAccount recipient;
+
+    TransferService transferService(sender, recipient);
+
+    // Ensure that no interactions occur with the BankAccount objects
+    EXPECT_CALL(sender, getBalance()).Times(0);
+    EXPECT_CALL(sender, withdraw(_)).Times(0);
+    EXPECT_CALL(recipient, deposit(_)).Times(0);
+
+    // Expect std::runtime_error to be thrown for a negative transfer amount
+    EXPECT_THROW(transferService.transfer(negativeTransferAmount), std::runtime_error);
 
 }
